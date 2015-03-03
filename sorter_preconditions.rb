9e272596126e6a9a56407e6b94a22c1b801c6b8a
kdbanman <kdbanman@ualberta.ca>
2015-03-03 11:29:29 -0700

    include length and indexability

--- sorter_preconditions.rb
+++ sorter_preconditions.rb
@@ -12,6 +12,22 @@ module SorterPreconditions
     raise ContractFailure, msg if unsorted.any? { |element| !element.respond_to? :<=>}
   end
 
+  def hasLength(unsorted)
+    msg = "Container must respond to .length, .size, or .count with an Integer"
+
+    length_response = (unsorted.respond_to? :length) && (unsorted.length.is_a? Integer)
+    size_response = (unsorted.respond_to? :size) && (unsorted.size.is_a? Integer)
+    count_response = (unsorted.respond_to? :count) && (unsorted.count.is_a? Integer)
+
+    raise ContractFailure, msg unless length_response || size_response || count_response
+  end
+
+  def indexMutable(unsorted)
+    msg = "Container must respond to [] and []="
+
+    raise ContractFailure, msg unless (unsorted.respond_to? :[]) && (unsorted.respond_to? :[]=)
+  end
+
   def booleanOrder(ascending)
     msg = "Ascending sort order must be true or false"
     raise ContractFailure, msg unless !!ascending == ascending
