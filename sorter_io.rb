9a23e95bb51515a2e3e9230ab8d421b772808cdb
ryant26 <ryant26@gmail.com>
2015-03-08 00:46:26 -0700

    Updated comments

--- sorter_io.rb
+++ sorter_io.rb
@@ -36,13 +36,16 @@ module SorterIO
 
     list = string.split("\n")
     out = []
+
+    #This ugly loop is so that we can make sure the objects are comparable to one another
+    #while we are walking over the string array. (no overhead)
     (0...list.length).each do |i|
       if /[-]?\d+(\.\d+)/.match(list[i]) #float
         out << list[i].to_f
-      elsif /[-]?\d+/.match(list[i])
+      elsif /[-]?\d+/.match(list[i])  #int
         out << list[i].to_i
       else
-        out << list[i]
+        out << list[i]  #push it on, and let the comparable check decide if we are good
       end
       isComparable(i, out)
     end
