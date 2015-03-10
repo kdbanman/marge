b4fa70d8243e8fb3aa572c21a81dbe174b2fa229
Ryan <thornhil@e5-05-03.ece.ualberta.ca>
2015-03-10 12:49:58 -0600

    Data Generator takes an argument

--- data_generator.rb
+++ data_generator.rb
@@ -2,8 +2,8 @@ file = open("./test.txt", 'w')
 
 rnd = Random.new
 
-(100).times do |i|
-	file << rnd.rand(0..1000).to_s + "\n"
+(ARGV[0].to_i).times do |i|
+	file << rnd.rand(0..ARGV[0].to_i * 10).to_s + "\n"
 end
 
 file.close
